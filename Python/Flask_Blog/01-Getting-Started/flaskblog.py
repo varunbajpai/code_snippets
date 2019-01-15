d4a57f6a82ce5b374f5bb4989a9f3b432e09b77c
varunbajpai <35426055+varunbajpai@users.noreply.github.com>
2019-01-15 12:30:54 +0530

    Update flaskblog.py

--- Python/Flask_Blog/01-Getting-Started/flaskblog.py
+++ Python/Flask_Blog/01-Getting-Started/flaskblog.py
@@ -1,17 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__)                           #This will create an object of app which we will run later
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/")                                 #Setting up routes is so easy in django
+@app.route("/home")                             #the '/' and '/home' wil be returning the same html page as shown
 def home():
     return "<h1>Home Page</h1>"
 
 
-@app.route("/about")
+@app.route("/about")                            #this is setting up the '/about' route as shown
 def about():
     return "<h1>About Page</h1>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port = 5000,debug=True).  #This will bind the host to the current ip on the port 5000
